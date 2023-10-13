b037af7f87c9830e813c4859fef97e192eed0daa
Aryan Bamane <aryanbamane2@gmail.com>
2023-10-13 15:04:36 +0530

    Updating Files

--- Task 2/main.js
+++ Task 2/main.js
@@ -1,24 +1,17 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './components/about.css'
+import './components/contact.css'
+import './components/footer.css'
+import './components/header.css'
+import './components/intro.css'
+import './components/projects.css'
+import './components/skills.css'
+import './components/style.css'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+let hamburger = document.querySelector(".hamburger");
+let header = document.querySelector("header");
+let nav = document.querySelector("nav");
+hamburger.addEventListener("click", () => {
+  header.classList.toggle("expanded");
+  nav.classList.toggle("active");
+})
